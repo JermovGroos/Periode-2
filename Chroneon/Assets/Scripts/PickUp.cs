54d8e884c376c59a9567e48f55ed28bceba4e256
JermovGroos <jvgroos@hotmail.nl>
2017-12-19 13:40:07 +0100

    Chroneon
    Chroneon files from 12/19/2017

---
+++ Chroneon/Assets/Scripts/PickUp.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour {
+	public Vector3 spawnPos;
+	public GameObject g;
+	public int i;
+	public Vector3 RotateVector;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+
+	void OnCollisionEnter (Collision c){ 
+
+		if (c.gameObject.name == "Hand")
+		{
+			c.gameObject.GetComponent<ClassMovement> ().score += 1;
+
+			Instantiate (g, spawnPos, Quaternion.identity); 
+			Destroy(gameObject);
+
+
+		
+		}
+	}
+}
