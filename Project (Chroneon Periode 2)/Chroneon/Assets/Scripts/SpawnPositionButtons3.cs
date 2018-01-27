6ba78f39ed19b5b30fca42cf892e0bc3f853431a
JermovGroos <jvgroos@hotmail.nl>
2018-01-27 15:03:47 +0100

    Periode 2 tot nu
    alles wat af is tot nu toe

---
+++ Project (Chroneon Periode 2)/Chroneon/Assets/Scripts/SpawnPositionButtons3.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPositionButtons3 : MonoBehaviour {
+	public GameObject g;
+	public Vector3 spawnPos;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnCollisionEnter (Collision c) {
+		if (c.gameObject.name == "ButtonCube") {
+			spawnPos.x = (-17.8f);
+			spawnPos.y = (22.17f);
+			spawnPos.z = (-32);
+			Instantiate (g, spawnPos, transform.rotation);
+			Destroy (gameObject);
+       }
+	}
+}
---
+++ Project (Chroneon Periode 2)/RayTest/Assets/ArrayTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrayTest : MonoBehaviour {
+	public int [] i;
+	public List <int> il = new List<int> ();
+	public bool []j;
+
+	// Use this for initialization
+	void Start () {
+		i [2] = 20;
+		if( i[2]==20){
+			print ("hoi");
+		}
+		il.Add (1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
